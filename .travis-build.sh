68b09a33636b16d2a91248918212d9660818f031
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-10-15 14:44:19 -0700

    Build on OS X, silent mode

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 set -e # exit on errors
+
+function finish {
+    for a in `find logs -name "*.log"`; do
+        echo "###### $a ######"
+        cat $a
+    done
+}
+
+trap finish EXIT
+
 export MAKE="make -j2"
-make
+make V=0
