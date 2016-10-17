ac4b8dda6d2ddd846e7f6b7b478a8dbe6a34570c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-10-16 22:07:41 -0700

    Fixup

--- .travis-build.sh
+++ .travis-build.sh
@@ -14,5 +14,5 @@ export MAKE="make -j4"
 
 export SAGE_INSTALL_CCACHE=yes
 
-make V=0 SAGE_PV="PYTHONUNBUFFERED=1 sage-progress-meter" $TARGET
+make V=0 SAGE_PV="env PYTHONUNBUFFERED=1 sage-progress-meter" $TARGET
 
