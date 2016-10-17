cdea98e2d595613b87b55eb275659433f98ff9eb
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2016-10-16 21:45:33 -0700

    Use poor person's pv

--- .travis-build.sh
+++ .travis-build.sh
@@ -14,18 +14,5 @@ export MAKE="make -j4"
 
 export SAGE_INSTALL_CCACHE=yes
 
-function hungry_spinner {
-    function kill_spinner {
-	kill $SPINNER_PID
-    }
-    trap kill_spinner EXIT 
-    trap kill_spinner SIGPIPE 
-    trap kill_spinner SIGINT 
-    ( sleep 6; while true; do echo -n . >/dev/tty ; sleep 6; done ) &
-    SPINNER_PID=$!
-    cat
-};
-export -f hungry_spinner
-
-make V=0 SAGE_PV="hungry_spinner" $TARGET
+make V=0 SAGE_PV="sage-progress-meter" $TARGET
 
